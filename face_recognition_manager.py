1480495992141e29616d40eaa99314cdd9ebecb4
jebin2 <jebineinstein@gmail.com>
2025-08-23 15:34:49 +0530

    new ch

--- face_recognition_manager.py
+++ face_recognition_manager.py
@@ -1,6 +1,8 @@
 import cv2
 import face_recognition
 from detect_manager import DetectManager
+import time
+import numpy as np
 
 class FaceRecognitionManager(DetectManager):
 	"""
@@ -10,13 +12,20 @@ class FaceRecognitionManager(DetectManager):
 	def __init__(self, model='cnn'):
 		super().__init__()
 		self.model = model  # 'hog' or 'cnn'
-		self.loaded = True  # face_recognition does not require heavy loading
+		self.loaded = False
+		self._load_model()
 
 	def _load_model(self):
 		"""
-		No heavy model to load for face_recognition; placeholder for interface consistency.
+		Pre-warm dlib by running a dummy detection.
 		"""
-		print("FaceRecognitionManager ready.")
+		print("Loading FaceRecognition model (%s)...", self.model)
+		start = time.time()
+		# Run a dummy detection with a 1x1 image to warm up dlib & CUDA
+		dummy = cv2.cvtColor((255 * np.ones((10, 10, 3), dtype=np.uint8)), cv2.COLOR_BGR2RGB)
+		face_recognition.face_locations(dummy, model=self.model)
+		self.loaded = True
+		print("Model loaded in %.2fs", time.time() - start)
 
 	def _detect_frame(self, frame):
 		"""
@@ -40,3 +49,40 @@ class FaceRecognitionManager(DetectManager):
 		Nothing to cleanup for face_recognition, included for interface consistency.
 		"""
 		print("FaceRecognitionManager cleanup done.")
+
+if __name__ == "__main__":
+	import sys
+	import json
+	from tqdm import tqdm
+	if len(sys.argv) < 2:
+		print("Usage: python face_recognition_manager.py <image_path>")
+		sys.exit(1)
+
+	images_path = sys.argv[1]
+	with  open(images_path, 'r') as f:
+		images_path_obj = json.load(f)
+	detect_manager = FaceRecognitionManager()
+
+	for obj in tqdm(images_path_obj, desc="Detecting Faces"):
+		frame = cv2.imread(obj["frame_path"])
+		if "face_location" not in obj:
+			obj.update({
+				"face_location": None
+			})
+		if "already_processed_face" not in obj:
+			obj.update({
+				"already_processed_face": False
+			})
+
+		if not obj["already_processed_face"]:
+			result = detect_manager._detect_frame(frame)
+			obj.update({
+				"already_processed_face": True
+			})
+			if result:
+				obj.update({
+					"face_location": result[0]['bbox']
+				})
+
+			with open(images_path, 'w') as f:
+				json.dump(images_path_obj, f, indent=4)
